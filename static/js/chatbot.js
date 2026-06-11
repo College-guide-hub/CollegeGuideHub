0d74abf855fa6810a9cb2c2f04182a437f23d018
College-guide-hub <lakshmi.01tech@gmail.com>
2026-06-11 17:21:55 +0100

    Update chatbot.js

--- static/js/chatbot.js
+++ static/js/chatbot.js
@@ -1,1 +1,28 @@
-console.log("Chatbot Loaded");
+async function sendQuestion() {
+
+    let question =
+    document.getElementById("question").value;
+
+    let response =
+    await fetch("/chat", {
+
+        method: "POST",
+
+        headers: {
+            "Content-Type":
+            "application/json"
+        },
+
+        body: JSON.stringify({
+            question: question
+        })
+    });
+
+    let data =
+    await response.json();
+
+    document.getElementById(
+        "answer"
+    ).innerHTML =
+    data.answer;
+}
