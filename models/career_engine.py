fe095abaf898788468d9a5a3fdf38e55364a83db
College-guide-hub <lakshmi.01tech@gmail.com>
2026-06-11 16:58:24 +0100

    Create career_engine.py

---
+++ models/career_engine.py
@@ -0,0 +1,12 @@
+def suggest_career(skill):
+
+    if skill.lower() == "ai":
+        return "Machine Learning Engineer"
+
+    if skill.lower() == "web":
+        return "Full Stack Developer"
+
+    if skill.lower() == "cyber":
+        return "Cyber Security Analyst"
+
+    return "Software Engineer"
