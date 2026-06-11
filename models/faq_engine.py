38f1043d9933bfe0e214b06a57354296afb256b4
College-guide-hub <lakshmi.01tech@gmail.com>
2026-06-11 17:17:50 +0100

    Update faq_engine.py

--- models/faq_engine.py
+++ models/faq_engine.py
@@ -1,2 +1,14 @@
+import pandas as pd
+
+faq = pd.read_csv("datasets/faq.csv")
+
 def get_answer(question):
-    return "Hello Student"
+
+    question = question.lower()
+
+    for _, row in faq.iterrows():
+
+        if row["question"].lower() in question:
+            return row["answer"]
+
+    return None
